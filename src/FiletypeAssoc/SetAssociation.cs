056a92f13502f79de5e8e02f34887705b7fa1a12
sebl <sebl@schnellebuntebilder.de>
2020-06-02 14:14:03 +0200

    adds SetAssociation Node

---
+++ src/FiletypeAssoc/SetAssociation.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FiletypeAssoc
+{
+    public class SetFileAssociations
+    {
+        /// <summary>
+        /// needed so that Explorer windows get refreshed after the registry is updated
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <param name="flags"></param>
+        /// <param name="item1"></param>
+        /// <param name="item2"></param>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImport("Shell32.dll")]
+        private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
+
+        private const int SHCNE_ASSOCCHANGED = 0x8000000;
+        private const int SHCNF_FLUSH = 0x1000;
+
+        /// <summary>
+        /// Set a FileType Association
+        /// </summary>
+        /// <param name="extension"></param>
+        /// <param name="progId"></param>
+        /// <param name="fileTypeDescription"></param>
+        /// <param name="applicationFilePath"></param>
+        /// <returns></returns>
+        public static bool SetAssociation(string extension, string progId, string fileTypeDescription, string applicationFilePath)
+        {
+            bool madeChanges = false;
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + extension, progId);
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + progId, fileTypeDescription);
+            madeChanges |= SetKeyDefaultValue($@"Software\Classes\{progId}\shell\open\command", "\"" + applicationFilePath + "\" \"%1\"");
+
+            if (madeChanges)
+            {
+                SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSH, IntPtr.Zero, IntPtr.Zero);
+            }
+
+            return madeChanges;
+        }
+
+        private static bool SetKeyDefaultValue(string keyPath, string value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(keyPath))
+            {
+                if (key.GetValue(null) as string != value)
+                {
+                    key.SetValue(null, value);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
